f8cbd11be26676363f9eb6b7c08861f1a7a75ba8
gywn908 <henry.bloch.04@gmail.com>
2025-02-13 11:34:59 -0600

    Added working motor code
    Added motor code that has threading and is working outside of container.

---
+++ motor_tester/motor_listener.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+'''
+ROS 
+---
+
+node:
+-----
+    - motor_listener
+
+Publishes:
+----------
+
+Subscribes:
+----------
+    - motor_command
+
+'''
+
+
+# Begin typing imports
+from typing import List
+# End typing imports
+
+# remove
+from motor_command_local.motor_interface import MotorInterface
+import time
+# remove 
+import threading
+
+local_channels: List[int] = [x for x in range(8)]
+num_motors: int = len(local_channels)
+motor_caller = MotorInterface(local_channels, num_motors, 0, 100, .1, .5, 5)
+
+num_motors = 8
+high: List[int] = [20 for _ in range(num_motors)]
+low: List[int] = [30 for _ in range(num_motors)]
+list_thing = high
+hl_counter = 0
+
+def arm_motors():
+    motor_caller.arm_seq()
+
+arm_thread = threading.Thread(target=arm_motors, daemon=True)
+arm_thread.start()
+
+try:
+    while (True):
+        list_thing = high if hl_counter == 1 else low
+        hl_counter = (hl_counter + 1) % 2
+        motor_caller.callback(list_thing)
+        time.sleep(5)
+except KeyboardInterrupt:
+    pass
+finally:
+    motor_caller.clo_seq()
+    if arm_thread.is_alive():
+        arm_thread.join(timeout=2)
