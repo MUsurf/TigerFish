08b8518d00b49db24d65f829433e7cf8f58f93b4
Max Welge <mdwelge@gmail.com>
2025-04-24 18:38:13 -0500

    SurfBoard Outputs

--- state_machine/state_machine/input_output.py
+++ state_machine/state_machine/input_output.py
@@ -1,7 +1,27 @@
-class SurfBoard:
-    #Stuff
-    pass
+class SurfBoard: # input
+    #TODO 
+    #gotta finish i think
+    # so this is what the surfboard needs to take in
+    x : float 
+    y : float
+    z : float 
+    roll : float 
+    pitch : float 
+    yaw : float 
+    cameraAngleTop : float
+    cameraAngleBot : float
+    time : float
+    SubPower: float
 
 class Outputs: 
-    #Stuff
-    pass
+    Last5SecondsDepth : list
+    #might be useful to have the last 5 seconds of depth
+    #TODO 
+    # so this is what the surfboard outputs
+    x : float 
+    y : float
+    z : float 
+    roll : float 
+    pitch : float 
+    yaw : float 
+
