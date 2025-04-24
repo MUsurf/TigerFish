59e723c1f476c5b8426f1a055aeb8927e55cc057
Creeon <creeon99@gmail.com>
2025-04-24 18:11:35 -0500

    Added init for state machine

--- state_machine/state_machine/state_machine.py
+++ state_machine/state_machine/state_machine.py
@@ -1,3 +1,12 @@
+from input_output import SurfBoard, Outputs
+from supervisor import Supervisor
+from task import Task
+
 class StateMachine:
-    #Stuff
-    pass
+    def __init__(self):
+        self.publisher : None = None # Make this a ROS publisher 
+        self.tasks : list = []
+        self.surf_board : SurfBoard = SurfBoard()
+        self.supervisor : Supervisor = Supervisor()
+        
+        
