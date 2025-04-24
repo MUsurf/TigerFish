e7b9a4f48e5cad65164b9a179bf9e49fa71f795d
Creeon <creeon99@gmail.com>
2025-04-24 18:25:18 -0500

    Added more state machine code

--- state_machine/state_machine/state_machine.py
+++ state_machine/state_machine/state_machine.py
@@ -1,12 +1,41 @@
 from input_output import SurfBoard, Outputs
 from supervisor import Supervisor
 from task import Task
+import time
 
 class StateMachine:
     def __init__(self):
         self.publisher : None = None # Make this a ROS publisher 
-        self.tasks : list = []
+        self.tasks : list = [None]
+        self.current_task : int = 0
+        self.last_error = None
+        self.MAX_ERROR_TIME = 5
+        
+        # Tasks adding here
+        
         self.surf_board : SurfBoard = SurfBoard()
         self.supervisor : Supervisor = Supervisor()
         
+    def execute(self):
+        output : Outputs = self.tasks[self.current_task].execute(self.surf_board)
+        output, bad_output = self.supervisor.check(output)
+        
+        # Error handling
+        if(bad_output):
+            if self.last_error == None : self.last_error = time.time()
+            
+            elif(time.time() - self.last_error >= self.MAX_ERROR_TIME):
+                #Prob should be adjusted
+                self.tasks[self.current_task].kill()
+                self.current_task = 0
+                self.last_error = None
+        else:
+            self.last_error = None
+            
+        self.publisher.publish(output)
+        
+        
+        
+        
+    
         
