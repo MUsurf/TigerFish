9a3ef210e1b242e89eb127d15673de9d984f89db
Kaden C <63522451+Kadition@users.noreply.github.com>
2025-04-29 18:59:53 -0500

    enter gate task started

--- state_machine/state_machine/task.py
+++ state_machine/state_machine/task.py
@@ -44,9 +44,48 @@ class EnterGate(Task):
     
     def execute(self):
         
-        # TODO do the stuff here 
+        # static have_captured_image
+        # 
 
-        pass
+        if PID.Rotating == false:
+
+            PID.beginRotationYaw()
+            return
+        
+        if Camera.see_gate and not Camera.Alligned:
+            
+            if(Camera.hasSeenGate):
+
+                PID.stopRotationYaw()
+
+            PID.allignYaw(Camera.needed())
+            return
+
+
+        # while true: (note: maybe this should be a time constraint? E.G. while not 5 minutes have past)
+        #   move to align gate
+        #   if gate_aligned:
+        #       stop moving
+        #       break
+        # if not have_captured_image:
+        #
+        #   chose_gate = choose_gate()
+        #
+        #   capture_image()
+        #
+        #   process_and_store_image()
+        #
+        # have_captured_image = true
+        #
+        # while true: (note: maybe this should be a time constraint? E.G. while not 5 minutes have past)
+        #   move towards chose_gate
+        #   if through_gate:
+        #       stop moving
+        #       break
+        #
+        # done
+
+        return
 
 class Slalom(Task):
     def __init__(self, type):
