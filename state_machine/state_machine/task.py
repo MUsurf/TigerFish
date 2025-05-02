da3dbf1ec6200e5c4c39795700a249dcd5c8d730
Kaden C <63522451+Kadition@users.noreply.github.com>
2025-05-01 19:26:21 -0500

    pair programming with Harry

--- state_machine/state_machine/task.py
+++ state_machine/state_machine/task.py
@@ -22,29 +22,114 @@ class EnterGate(Task):
     class State(enum.Enum): # enums for the internal task states
         seekingGate = 1
         movingTowardsGate = 2
+        barrelRolling = 3
+        movingThroughGate = 4
 
     def __init__(self, type):
         super().__init__(type)
         self.currentState : enum.Enum = self.State.seekingGate # Dr. Seuss
+        self.barrelRollCount : int = 0
+        self.angleAllowance : float = 5
+        self.currentAngleGoal : float = 10
 
     
     def execute(self):
 
         if self.isActive == False:
-
+            self.attempted = True
+            self.currentState = self.State.seekingGate # this should in theory never matter, but maybe supervisor would make this be important
             self.isActive = True
             return
         
         if self.currentState == self.State.seekingGate and not Camera.seeGate():
             
-            PID.start(0, 0, 0, 0, 0, 5)
+            # this is for yaw, as it is x, y, z, roll, pitch, yaw
+            # this is so we can find the gate as we arent facing it at the start
+            PID.start(0, 0, 0, 0, 0, 5) # velocity
             return
         
-        if Camera.seeGate() and not Camera.isAlligned():
+        if self.currentState == self.State.seekingGate and not Camera.allignedYaw():
 
-            PID.start(0, 0, 0, 0, 0, Camera.neededYaw())
+            # this is a call to the PID to allign the robot to the gate
+            PID.start(0, 0, 0, 0, 0, Camera.neededYaw()) # position or velocity
+            return
+        
+        if self.currentState == self.State.seekingGate and not Camera.allignedPosition():
+            
+            # this is a call to the PID to allign the robot to the gate
+            PID.start(0, Camera.neededY(), 0, 0, 0, 0) # position or velocity
             return
 
+        if self.currentState == self.State.seekingGate:
+            
+            self.currentState = self.State.movingTowardsGate # state change to start the process of going through the gate
+            PID.stop() # short hand for PID.start(0, 0, 0, 0, 0, 0)
+            return
+
+        if self.currentState == self.State.movingTowardsGate and not Camera.withinWantedDistance():
+            
+            PID.start(5, 0, 0, 0, 0, 0) # move towards gate also is velocity
+            return
+
+        if self.currentState == self.State.movingTowardsGate and (not Camera.allignedYaw() or not Camera.allignedPosition()):
+            
+            self.currentState = self.State.seekingGate
+            return
+        
+        if self.currentState == self.State.movingTowardsGate:
+
+            if(self.barrelRollCount < 3):
+
+                self.currentState = self.State.barrelRolling
+
+            else:
+
+                self.currentState = self.State.movingThroughGate
+
+            PID.stop()
+            return
+        
+        
+        if self.currentState == self.State.barrelRolling and self.barrelRollCount < 3:
+            
+            # ! implement something that increases the velocity of the roll when the later check fails
+            PID.start(0, 0, 0, 5, 0, 0) # velocity
+
+            if self.currentAngleGoal < PID.subRollAngle() < self.currentAngleGoal + self.angleAllowance:
+                
+                self.currentAngleGoal = PID.subRollAngle() + self.angleAllowance
+
+                if self.currentAngleGoal > 360:
+
+                    self.currentAngleGoal = self.currentAngleGoal % 360
+
+                    self.barrelRollCount += 1
+            return
+
+        if self.currentState == self.State.barrelRolling:
+
+            self.currentState = self.State.seekingGate
+            return
+        
+        if self.currentState == self.State.movingThroughGate and Camera.seeGate():
+
+            PID.start(5, 0, 0, 0, 0, 0) # velocity
+            return
+        
+
+        if self.currentState == self.State.movingThroughGate:
+
+            self.isComplete = True
+
+            # if it gets to here, transition to another state
+
+            # also, should we reset state here??? (aka put the vars back to their original)
+
+        # if it gets to here, something bad happened as it failed all checks, which shoulnt happen
+
+        
+
+        
 
         # while true: (note: maybe this should be a time constraint? E.G. while not 5 minutes have past)
         #   move to align gate
