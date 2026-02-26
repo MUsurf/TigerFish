940459b3c2ef44e6506505eb285546c5407d4a22
Darkskymoon <108477682+Darkskymoon@users.noreply.github.com>
2026-02-25 18:19:42 -0600

    added messaging stuffs

--- process_images/src/process_img_node.cpp
+++ process_images/src/process_img_node.cpp
@@ -4,8 +4,8 @@
 #include "process_images/img_preprocessing.hpp"
 #include "sensor_msgs/msg/image.hpp"
 #include "cv_bridge/cv_bridge.hpp"
-#include "process_images/img_preprocessing.hpp"
 #include "process_images/marker_detector.hpp"
+#include "tiger_fish_msgs/msg/marker_detection.hpp"
 
 
 class processImgNode : public rclcpp::Node{
@@ -31,7 +31,7 @@ public:
                 std::bind(&processImgNode::image_callback, this, std::placeholders::_1));
 
             // Create a publisher for marker data
-            // marker_pub_ = this->create_publisher<tiger_fish_msgs::msg::MarkerDetection>("vision/marker_data", 10);
+            marker_pub_ = this->create_publisher<tiger_fish_msgs::msg::MarkerDetection>("vision/marker_data", 10);
 
             //logging
     RCLCPP_INFO(this->get_logger(), "processImgNode ready for images...");
@@ -122,13 +122,14 @@ private:
       MarkerResult marker_result = marker_detector_->find_markers(processed_frame);
       marker_detector_->visualize_markers(processed_frame, marker_result);
 
-      // auto marker_msg = tiger_fish_msgs::msg::MarkerDetection();
-      // marker_msg.found = marker_result.found;
-      // marker_msg.x = marker_result.norm_position.x;
-      // marker_msg.y = marker_result.norm_position.y;
-      // marker_msg.depth = marker_result.depth;
-      // marker_msg.angle = marker_result.angle;
-      // marker_pub_->publish(marker_msg);
+      auto marker_msg = tiger_fish_msgs::msg::MarkerDetection();
+      marker_msg.header = msg->header;
+      marker_msg.found = marker_result.found;
+      marker_msg.x = marker_result.norm_position.x;
+      marker_msg.y = marker_result.norm_position.y;
+      marker_msg.depth = marker_result.depth;
+      marker_msg.angle = marker_result.angle;
+      marker_pub_->publish(marker_msg);
 
                 //stitch images
       cv::Mat combined_frame = StitchImg(raw_frame, processed_frame);
@@ -155,7 +156,7 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
   std::unique_ptr<MarkerDetector> marker_detector_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
-  // rclcpp::Publisher<tiger_fish_msgs::msg::MarkerDetection>::SharedPtr marker_pub_;
+  rclcpp::Publisher<tiger_fish_msgs::msg::MarkerDetection>::SharedPtr marker_pub_;
 
 
   cv::VideoWriter video_writer_;
