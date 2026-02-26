e758925d20586d00d5a86dc33a64c3dc60bbd01f
Darkskymoon <108477682+Darkskymoon@users.noreply.github.com>
2026-02-26 17:59:10 -0600

    fixed build error

--- process_cameras/setup.py
+++ process_cameras/setup.py
@@ -1,38 +1,35 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'ros2_opencv'
+package_name = "ros2_opencv"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Raquel',
-    maintainer_email='rrfgvd@umsystem.edu',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer="Raquel",
+    maintainer_email="rrfgvd@umsystem.edu",
+    description="TODO: Package description",
+    license="TODO: License declaration",
     extras_require={
-        'test': [
-            'pytest',
+        "test": [
+            "pytest",
         ],
     },
     entry_points={
-        'console_scripts': ['publisher_node=ros2_opencv.cameraPublisher:main',
-        'subscriber_node=ros2_opencv.subscriberImage:main',
-        'servo_controller = servo_controller.servo_controller:main'
+        "console_scripts": [
+            "publisher_node=ros2_opencv.cameraPublisher:main",
+            "subscriber_node=ros2_opencv.subscriberImage:main",
+            "servo_controller = servo_controller.servo_controller:main",
         ],
     },
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
 )
--- process_imu/process_imu/imu.py
+++ process_imu/process_imu/imu.py
@@ -1,4 +1,3 @@
-
 # BEGIN IMPORT
 import time
 import rclpy
@@ -7,7 +6,6 @@ from std_msgs.msg import Float32MultiArray
 import threading
 # END IMPORT
 
-from typing import List
 
 import board
 import busio
@@ -16,8 +14,7 @@ from rclpy.qos import QoSProfile, ReliabilityPolicy
 
 qos = QoSProfile(depth=1, reliability=ReliabilityPolicy.BEST_EFFORT)
 
-POLLING_RATE = 40 # Hz
-
+POLLING_RATE = 40  # Hz
 
 
 # Decleration of wrapper for threading a function
@@ -26,16 +23,18 @@ def threaded(fn):
         thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
         thread.start()
         return thread
+
     return wrapper
 
+
 class IMUTracker(Node):
-    def __init__(self, polling_rate : float):
-        super().__init__('imu_tracker')
+    def __init__(self, polling_rate: float):
+        super().__init__("imu_tracker")
         self.polling_rate = polling_rate
         # Initialize I2C and BNO055 sensor
         i2c = busio.I2C(board.SCL, board.SDA)
         self.sensor = adafruit_bno055.BNO055_I2C(i2c, address=0x28)
-        
+
         # Current roll, pitch, yaw
         self.roll = 0.0
         self.pitch = 0.0
@@ -52,32 +51,27 @@ class IMUTracker(Node):
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
-        
+
         self._last_update_time = time.time()
-        
+
         self.stop_event = threading.Event()
-        
-        self.publisher = self.create_publisher(
-            Float32MultiArray,
-            'imu_data',
-            qos
-        )
+
+        self.publisher = self.create_publisher(Float32MultiArray, "imu_data", qos)
         self.polling_thread = self.polling_function()
 
     def update(self):
         """Read current Euler angles from the BNO055."""
         euler = self.sensor.euler  # Returns (heading, roll, pitch)
         acc = self.sensor.acceleration  # Returns (x, y, z)
-        ang_vel = self.sensor.gyro  
+        ang_vel = self.sensor.gyro
         if euler is None or acc is None:
             # Sensor not ready yet
             return
-        
+
         self.yaw, self.roll, self.pitch = euler
         self.vroll, self.vpitch, self.vyaw = ang_vel
         self.ax, self.ay, self.az = acc
-        
-        
+
         dt = time.time() - self._last_update_time
         self._last_update_time = time.time()
         self.vx += self.ax * dt
@@ -86,36 +80,40 @@ class IMUTracker(Node):
         self.x += self.vx * dt
         self.y += self.vy * dt
         self.z += self.vz * dt
-        
+
     @threaded
     def polling_function(self):
         while not self.stop_event.is_set():
             self.update()
-            self.publisher.publish(Float32MultiArray(data=[
-                self.roll,
-                self.pitch,
-                self.yaw,
-                self.vroll,
-                self.vpitch,
-                self.vyaw,
-                self.ax,
-                self.ay,
-                self.az,
-                self.vx,
-                self.vy,
-                self.vz,
-                self.x,
-                self.y,
-                self.z
-            ]))
-            if self.stop_event.wait(timeout = 1.0 / self.polling_rate):
+            self.publisher.publish(
+                Float32MultiArray(
+                    data=[
+                        self.roll,
+                        self.pitch,
+                        self.yaw,
+                        self.vroll,
+                        self.vpitch,
+                        self.vyaw,
+                        self.ax,
+                        self.ay,
+                        self.az,
+                        self.vx,
+                        self.vy,
+                        self.vz,
+                        self.x,
+                        self.y,
+                        self.z,
+                    ]
+                )
+            )
+            if self.stop_event.wait(timeout=1.0 / self.polling_rate):
                 break
-            
+
     def shutdown(self):
         self.get_logger().info("Shutting down IMU tracker")
-                
+
         self.stop_event.set()
-        
+
         if self.polling_thread.is_alive():
             self.polling_thread.join(timeout=10)
 
@@ -133,6 +131,6 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-        
