5bed88c059a516524bcfee89c1ab25bf459b1ba3
Darkskymoon <108477682+Darkskymoon@users.noreply.github.com>
2026-02-26 18:26:28 -0600

    updated subscriber to write mp4s

--- process_cameras/ros2_opencv/subscriberImage.py
+++ process_cameras/ros2_opencv/subscriberImage.py
@@ -2,12 +2,15 @@
 
 # Import OpenCV
 import cv2
+import os
 
 # Import ROS2 package modules and libraries
 import rclpy
 from sensor_msgs.msg import Image
 from rclpy.node import Node
 from cv_bridge import CvBridge
+
+
 
 
 class SubscriberNodeClass(Node):
@@ -17,6 +20,8 @@ class SubscriberNodeClass(Node):
 
         # Convert OpenCV images to ROS2 msgs
         self.bridgeObject = CvBridge()
+        self.video_writer = None
+        self.output_path = "/home/ros2_ws/src/output_images/processed_vid/processed_output.mp4"
 
         # Name must match publisher node
         self.topicNameFrames = "topic_camera_image"
@@ -37,9 +42,25 @@ class SubscriberNodeClass(Node):
         # Convert ROS2 image msg to OpenCV image
         openCVImage = self.bridgeObject.imgmsg_to_cv2(imageMessage)
 
-        # Show image on screen
-        cv2.imshow("Camera video", openCVImage)
-        cv2.waitKey(1)
+        # Show image on screen - only activate this for testing :)
+        # cv2.imshow("Camera video", openCVImage)
+        # cv2.waitKey(1)
+
+        # this is initialization of a video writer to write video at 30 frames
+        # in mp4v format! UwU
+        if self.video_writer is None:
+            os.makedirs(os.path.dirname(self.output_path), exist_ok=True)
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v') # mpv4 format initialization
+
+            height, width = openCVImage.shape[:2]
+
+            self.video_writer= cv2.VideoWriter(
+                self.output_path,
+                fourcc,
+                30.0, # frames
+                (width, height) # frame size
+            )
+        self.video_writer.write(openCVImage) # write the video
 
 
 # Main function; entry point
