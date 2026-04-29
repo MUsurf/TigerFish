f73997b2b53870e463cc5d45df80d6af179d79fe
Raquel F <noodlesthebird@gmail.com>
2026-04-29 18:30:38 -0500

    Started pole alignment state

--- src/state_machine/src/PrequalMachine/gate_alignment.py
+++ src/state_machine/src/PrequalMachine/gate_alignment.py
@@ -24,9 +24,9 @@ class GateAlignment(State):
 
         Returns: next_state
         """
-        yasmin.YASMIN_LOG_INFO("Executing state START")
+        yasmin.YASMIN_LOG_INFO("Executing state Gate Alignment")
         gate = bb.get("gate_detection")
-        odom = bb.get("odom")
+        # odom = bb.get("odom")
         depth = bb.get("depth")
         desired_depth = bb.get("desired_depth")
         send = False
@@ -39,9 +39,15 @@ class GateAlignment(State):
             send = True
         if (self.screen_center[0] != gate["x_pos"]): #  and self.screen_center[1] != gate["y_pos"] 
             # TODO: Figure smth out with camera vis to do pixel distances
-            msg.y_measurement = odom["y"]
-            msg.y_setpoint = gate["x_pos"] # Not sure this will work since it is perceived distance on a screen and not real?
+            if ((self.screen_center[0] - gate["x_pos"]) > 0):
+                msg.y_power = 5
+            else:
+                msg.y_power = -5 # Allowed?
+            # msg.y_measurement = odom["y"]
+            # msg.y_setpoint = gate["x_pos"] # Not sure this will work since it is perceived distance on a screen and not real?
             send = True
+        else:
+            msg.y_power = 0
         if (send):
             self.context.pid_publisher.publish(msg)
         else:
--- src/state_machine/src/PrequalMachine/pole_alignment.py
+++ src/state_machine/src/PrequalMachine/pole_alignment.py
@@ -7,32 +7,56 @@ from yasmin import State
 
 class PoleAlignment(State):
     """
-    State we boot to in the state machine.
+    State to align with and go to pole.
     """
     def __init__(self) -> None:
         """
-        Initializes the StartState instance, setting up the outcomes.
+        Initializes the Pole Alignment instance, setting up the outcomes.
 
         Outcomes:
-            outcome1: Indicates the state should continue to the Gate state.
-            outcome2: Indicates the state should finish execution and return.
+            next_state: goes to the circle-pole-state
         """
-        super().__init__(["outcome1", "outcome2"])
-        self.set_description(
-            "Ensures proper boot, transitions to Gate state or checks fails and we quit?"
-        )
+        super().__init__(["next_state"])
         
         
-    def execute(self, blackboard: Blackboard):
+    def execute(self, bb: Blackboard):
         """
-        Executes the logic for the Start state.
-
-        Args:
-
-        Returns:
+        Executes the logic for the Pole Alignment state.
 
         Raises:
             Exception: May raise exceptions related to state execution.
         """
-        yasmin.YASMIN_LOG_INFO("Executing state START")
-        time.sleep(3)  # Simulate work by aw
+        yasmin.YASMIN_LOG_INFO("Executing state Pole Alignment")
+        pole = bb.get("pole_detection")
+        odom = bb.get("odom")
+        depth = bb.get("depth")
+        desired_depth = bb.get("desired_depth")
+        send = False
+        
+        msg = PIDInput()
+        
+        # Maintain depth
+        if (depth != desired_depth):
+            msg.z_measurement = depth
+            msg.z_setpoint = desired_depth
+            send = True
+            
+        # Center pole
+        if (self.screen_center[0] != pole["x_pos"]):
+            # TODO: Figure smth out with camera vis to do pixel distances
+            if ((self.screen_center[0] - pole["x_pos"]) > 0):
+                msg.y_power = 5
+            else:
+                msg.y_power = -5 # Allowed?
+            send = True
+        if (odom.y_measurement != 0):
+            msg.y_power = 0
+            send = True
+            
+        # Go to pole
+        # TODO: Use computer vis to keep a certain distance 
+        
+        if (send):
+            self.context.pid_publisher.publish(msg)
+        else:
+            return "next_state"
