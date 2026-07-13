b981fea7ca54e427984b66823df4d3f71b52fade
Kaden C <63522451+Kadition@users.noreply.github.com>
2026-07-13 16:38:41 -0700

    the uhhh path marker thing or whatever (henry said he loved it by the way)

---
+++ src/state_machine/state_machine/path_marker.py
@@ -0,0 +1,77 @@
+from abc import ABC, abstractmethod
+import math
+from messages.msg import PIDInput, VisionMessage
+
+# TODO - henry, just conform to what it do for real for real, cause uhh... its like the best for me
+        
+class PathMarkerSee(ABC):
+    def __init__(self, name):
+        self.name = name
+        self.start_yaw = 0
+        
+    @abstractmethod
+    def start(self, context : dict) -> None:
+        self.start_yaw = context["odom"]["yaw"]
+        return None
+    
+    @abstractmethod
+    def execute(self, context : dict) -> str | None:
+
+        msg = PIDInput()
+        odom = context["odom"]
+        msg.x_mode = True
+        msg.x_setpoint = marker_x_offset * 0.2 # TODO - hemmy, uhhh, this camera bottom, the .2 is so it dont go too fast
+        msg.measurement_x = 0.0
+        msg.y_mode = True
+        msg.y_setpoint = marker_y_offset * 0.2
+        msg.measurement_y = 0.0
+        msg.z_mode = True
+        msg.roll_mode = True
+        msg.pitch_mode = True
+        msg.yaw_mode = True
+        msg.pitch_setpoint = 0.0
+        msg.measurement_pitch = odom["pitch"]
+        msg.roll_setpoint = 0.0
+        msg.measurement_roll = odom["roll"]
+        msg.yaw_setpoint = self.start_yaw
+        msg.measurement_yaw = odom["yaw"]
+        self.context.pid_publisher.publish(msg)
+
+        if math.abs(marker_x_offset) < 5 and math.abs(marker_y_offset) < 5:
+            return PathMarkerSpin
+        
+class PathMarkerSpin(ABC):
+    def __init__(self, name):
+        self.name = name
+        self.start_yaw = 0
+        
+    @abstractmethod
+    def start(self, context : dict) -> None:
+        self.start_yaw = context["odom"]["yaw"]
+        return None
+        
+    @abstractmethod
+    def execute(self, context : dict) -> str | None:
+
+        msg = PIDInput()
+        odom = context["odom"]
+        msg.x_mode = True
+        msg.x_setpoint = marker_x_offset * 0.2 # TODO - hemmy, uhhh, this camera bottom, the .2 is so it dont go too fast
+        msg.measurement_x = 0.0
+        msg.y_mode = True
+        msg.y_setpoint = marker_y_offset * 0.2
+        msg.measurement_y = 0.0
+        msg.z_mode = True
+        msg.roll_mode = True
+        msg.pitch_mode = True
+        msg.yaw_mode = True
+        msg.pitch_setpoint = 0.0
+        msg.measurement_pitch = odom["pitch"]
+        msg.roll_setpoint = 0.0
+        msg.measurement_roll = odom["roll"]
+        msg.yaw_setpoint = 0.0
+        msg.measurement_yaw = yaw_angle_to_marker # TODO same thing as before but bottom NOTEE - this requires that you givr me the right direction, if not very bad
+        self.context.pid_publisher.publish(msg)
+
+        if math.abs(yaw_angle_to_marker) < 5:
+            return next_state
